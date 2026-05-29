2b1337c8d84b4154099d33ebcecdda5a30ef25f4
Rudy Moran <rudymoran18@gmail.com>
2026-05-29 13:14:45 -0600

    Corregir servicios para usar backend en producción

--- frontend/src/app/services/paciente.service.ts
+++ frontend/src/app/services/paciente.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Paciente } from '../models/paciente.model';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
